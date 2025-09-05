1dfe9c3db76b976781f0e2ca75d0598c2d2c1280
alextalijan <acatalijan@icloud.com>
2025-09-05 16:48:22 +0200

    style: make the scoreboard look legit

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,8 @@
 export default function Scoreboard({ score, bestScore }) {
   return (
-    <div>
-      <span>Score: {score}</span>
-      <span>Best Score: {bestScore}</span>
+    <div className="scoreboard">
+      <span>SCORE: {score}</span>
+      <span>BEST SCORE: {bestScore}</span>
     </div>
   );
 }
