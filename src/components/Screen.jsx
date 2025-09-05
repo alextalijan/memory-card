da007966ecbe0a77bb46a4a225250e8ab86b7e56
alextalijan <acatalijan@icloud.com>
2025-09-05 13:27:20 +0200

    feat: add logic that fetches and displays characters
    When the page is initially loaded, and only then are the characters fetched
    from Disney API, and returned in the form of Card components.

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,10 +1,10 @@
 import Card from './Card.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Screen() {
   const [characters, setCharacters] = useState([]);
 
-  function getRandomCharacters(array, amount = 16) {
+  function getRandomMembers(array, amount = 16) {
     // Shuffle using Fisher-Yates algorithm
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -15,28 +15,37 @@ function Screen() {
     return array.slice(0, amount);
   }
 
-  fetch('https://api.disneyapi.dev/character')
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-    })
-    .then((data) => {
-      const randomCharacters = getRandomCharacters(data).map((character) => {
-        return {
-          id: character['_id'],
-          name: character.name,
-          imageUrl: character.imageUrl,
-        };
+  useEffect(() => {
+    const abortController = new AbortController(); // Initialize AbortController
+    const signal = abortController.signal; // Get the signal
+
+    fetch('https://api.disneyapi.dev/character', { signal }) // Pass the signal
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+      })
+      .then((json) => {
+        const randomCharacters = getRandomMembers(json.data).map(
+          (character) => {
+            return {
+              id: character['_id'],
+              name: character.name,
+              imageUrl: character.imageUrl,
+            };
+          }
+        );
+
+        setCharacters(randomCharacters);
+      })
+      .catch((error) => {
+        console.log('API call failed:', error);
       });
 
-      setCharacters(randomCharacters);
-    })
-    .catch((error) => {
-      console.log('API call failed:', error);
-    });
+    return () => abortController.abort(); // Abort the fetch request
+  }, []);
 
   return (
     <div className="screen">
