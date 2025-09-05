29d5510b115740d3ebf7d7b3353b3b9c05ca0bbf
alextalijan <acatalijan@icloud.com>
2025-09-05 20:30:17 +0200

    feat: add character name formatter before storing locally
    Since the keys in local storage start with "img_", due to consistency and
    JS inability to let a key name have space, I added a formatter that takes
    the name and standardizes it before checking if it's already in local
    storage.

--- src/js/getCachedImage.js
+++ src/js/getCachedImage.js
@@ -1,12 +1,14 @@
 import generateImageUrl from './generateImageUrl.js';
 
 async function getCachedImage(name) {
-  const url = localStorage.getItem(`img_${name}`);
+  const formattedName = name.toLowerCase().split(' ').join('_');
+
+  const url = localStorage.getItem(`img_${formattedName}`);
   if (url) return url;
 
   const googleUrl = await generateImageUrl(name);
   if (googleUrl) {
-    localStorage.setItem(`img_${name}`, googleUrl);
+    localStorage.setItem(`img_${formattedName}`, googleUrl);
     return googleUrl;
   }
 
